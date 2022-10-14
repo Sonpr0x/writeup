07a793ecbae5bd8f3a101afb0b87ff4ccb07ee2d
RAW: L.PUPJSE <82546097+Sonpr0x@users.noreply.github.com>
2022-10-15 01:14:07 +0700

    DST mod fix

---
+++ test.lua
@@ -0,0 +1,39 @@
+# Fixed conflict combine mods by L.Puppjse (mod more pets + mod pet skills)
+# 11/2021
+-------------------------------------------------------------------------------
+--  Combat Avoidance
+
+local function _avoidtargetfn(self, target)
+    if target == nil or not target:IsValid() then
+        return false
+    end
+
+    local owner = self.inst.components.follower.leader
+    local owner_combat = owner ~= nil and owner.components.combat or nil
+    local target_combat = target.components.combat
+    if owner_combat == nil or target_combat == nil then
+        return false
+    elseif target_combat:TargetIs(owner)
+        or (target.components.grouptargeter ~= nil and target.components.grouptargeter:IsTargeting(owner)) then
+        return true
+    end
+
+    local distsq = owner:GetDistanceSqToInst(target)
+    if distsq >= COMBAT_SAFE_TO_WATCH_FROM_MAX_DIST_SQ then
+        -- Too far
+        return false
+    elseif distsq < COMBAT_TOO_CLOSE_DIST_SQ and target_combat:HasTarget() then
+        -- Too close to any combat
+        return true
+    end
+
+    -- Is owner in combat with target?
+    -- Are owner and target both in any combat?
+    local t = GetTime()
+    return  (   (owner_combat:IsRecentTarget(target) or target_combat:HasTarget()) and
+                math.max(owner_combat.laststartattacktime, owner_combat.lastdoattacktime or 0) + COMBAT_TIMEOUT > t
+            ) or
+            (   owner_combat.lastattacker == target and
+                owner_combat:GetLastAttackedTime() + COMBAT_TIMEOUT > t
+            )
+end
